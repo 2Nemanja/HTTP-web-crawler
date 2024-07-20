3fdc35b5aff6d9283ea010e10f0b20636339d493
2Nemanja <nemanjatrifunovic036@gmail.com>
2024-07-20 14:11:24 +0200

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,5 +1,38 @@
+const {JSDOM} = require('jsdom')
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const  urls = []
+    const epmty = ""
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for(const linkElement of linkElements) {
+        if(linkElement.href){ // checking if <a> isnt ""
+            if (linkElement.href.slice(0,1) === '/'){ // is the first caracter in the string a "/"
+                //relative
+               try {
+                    // using URL constructor to validate URL tha we got as input
+                    const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                    urls.push(urlObj.href) 
+               } catch (err) {
+                    console.log(`error with relative url: ${err.message}`)
+               }
+            } else {
+                //absolute
+                try {
+                    const urlObj = new URL(linkElement.href)
+                    urls.push(urlObj.href)
+                } catch (err) {
+                    console.log(`error with absolute url: ${err.message}`)
+                }
+            }
+        }
+    }
+    
+    return urls
+}
+
 function normalizeURL(urlString) {
-    const urlObj = new URL(urlString)
+    const urlObj   = new URL(urlString)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`
     if(hostPath.length > 0 && hostPath.slice(-1) === '/') { //.slice(-1) uzima poslednji karakter stringa
         return hostPath.slice(0,-1) //vraca sve sem poslednjeg karaktera
@@ -8,5 +41,6 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require('./crawl.js')
+const { normalizeURL, getURLsFromHTML } = require('./crawl.js')
 const { test, expect } = require('@jest/globals')
 
 //Ova fja sluzi kako bismo za vise razlicitih URL-ova koji pokazuju na isti sajt napravili normalizaciju 
@@ -31,5 +31,68 @@ test('normalized strip http', () => {
     const input = 'http://blog.boot.dev/path'
     const actual = normalizeURL(input)
     const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+
+test('normalized strip http', () => {
+    const input = 'http://blog.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+
+test('getURLsFromHTML absolute', () => { 
+    const inputHTMLbody = `
+    <html>
+        <body>
+            <a href="https://blog.boot.dev/path/">
+                Boot.dev Blog
+            <a>
+        <body>
+    <html>
+    `
+    const inputBaseURL = "https://blog.boot.dev/path/"
+    const actual = getURLsFromHTML(inputHTMLbody, inputBaseURL)
+    const expected = ["https://blog.boot.dev/path/"]
+    expect(actual).toEqual(expected)
+})
+
+
+test('getURLsFromHTML invalid', () => { 
+    const inputHTMLbody = `
+    <html>
+        <body>
+            <a href="unvalid">
+                Invalid URL
+            <a>
+        <body>
+    <html>
+    `
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLsFromHTML(inputHTMLbody, inputBaseURL)
+    const expected = [] // for invalid url it should return nothing
+    expect(actual).toEqual(expected)
+})
+
+
+
+test('getURLsFromHTML both', () => { 
+    const inputHTMLbody = `
+    <html>
+        <body>
+            <a href="https://blog.boot.dev/path1/">
+                Boot.dev Blog
+            <a>
+            <a href="/path2/">
+                Boot.dev Blog
+            <a>
+        <body>
+    <html>
+    `
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLsFromHTML(inputHTMLbody, inputBaseURL)
+    const expected = ["https://blog.boot.dev/path1/", "https://blog.boot.dev/path2/"]
     expect(actual).toEqual(expected)
 })
